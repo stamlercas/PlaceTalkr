a05d91626393151b0dd73276f859a5ae6b3ee0bc
Chris <stamlercas@yahoo.com>
2015-12-08 22:36:09 -0500

    Added styles and bug fixes

--- SocialMediaProject/app/src/main/java/com/stamler/socialmediaproject/LoginActivity.java
+++ SocialMediaProject/app/src/main/java/com/stamler/socialmediaproject/LoginActivity.java
@@ -66,21 +66,6 @@ public class LoginActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void authenticate(User user)
     {
         ServerRequests serverRequest = new ServerRequests(this);
